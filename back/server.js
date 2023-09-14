000bd943e926e5dbaa53cda610f37e7d4d23f81c
Melarc <melarc.ab@gmail.com>
2023-09-14 22:24:39 +0200

    added dockerfiles

--- back/server.js
+++ back/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const weatherRoutes = require('./routes/weather');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 9701;
 
 // Middleware
 app.use(cors());
